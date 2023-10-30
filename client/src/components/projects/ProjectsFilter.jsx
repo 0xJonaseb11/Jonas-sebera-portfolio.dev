1e9ca73e66a5499e1ff8aea58e3329c4884dde7c
jonas-sebera <sebejaz99@gmail.com>
2023-10-30 12:13:35 +0200

    filter projects

---
+++ client/src/components/projects/ProjectsFilter.jsx
@@ -0,0 +1,45 @@
+const selectOptions = [
+	'Web Application',
+	'Mobile Application',
+	'UI/UX Design',
+	'Web3.0 development',
+	'Smart contract development',
+	'Smart contract audit, Tests and deployment'
+];
+
+const ProjectsFilter = ({ setSelectProject }) => {
+	return (
+		<select
+			onChange={(e) => {
+				setSelectProject(e.target.value);
+			}}
+			className="font-general-medium 
+                px-4
+                sm:px-6
+                py-2
+                border
+                dark:border-secondary-dark
+                rounded-lg
+                text-sm
+                sm:text-md
+                dark:font-medium
+                bg-secondary-light
+                dark:bg-ternary-dark
+                text-primary-dark
+                dark:text-ternary-light
+            "
+		>
+			<option value="All Projects" className="text-sm sm:text-md">
+				All Projects
+			</option>
+
+			{selectOptions.map((option) => (
+				<option value={option} className="text-normal sm:text-md" key={option}>
+					{option}
+				</option>
+			))}
+		</select>
+	);
+};
+
+export default ProjectsFilter;
