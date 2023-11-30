f0520936ed621ab7aa9ead61609e976c07ce07bb
jonas-sebera <sebejaz99@gmail.com>
2023-11-30 04:48:16 +0200

    update aboutData

--- Jonas-sebera-portfolio/client/src/data/aboutMeData.js
+++ Jonas-sebera-portfolio/client/src/data/aboutMeData.js
@@ -1,23 +1,41 @@
 export const aboutMeData = [
 	{
 		id: 1,
+		title: "Introduction",
         bio: "Welcome to my world of innovative and transformative web3.0 development! I'm SEBERA JONAS, a seasoned full-stack web3.0 developer with a passion for bringing blockchain solutions to life. With a rich background in frontend, backend, and blockchain development, I specialize in crafting cutting-edge digital experiences that seamlessly integrate with decentralized technologies.",
 	},
 	{
 		id: 2,
+		title: "Frontend Expertise",
 		bio: `
 	
 
 		Frontend Excellence:
 		I excel in creating intuitive and visually stunning user interfaces that captivate and engage users. Leveraging the latest tools and frameworks, I ensure a seamless and responsive frontend experience, leaving a lasting impression on every user.
-		
+		`
+	},
+	{  id:3,
+		title: "Backend Mastery",
+		bio:
+		`	
 		Backend Mastery:
 		Behind every great frontend lies a robust backend, and that's where my expertise shines. I design and implement scalable, secure, and efficient server-side solutions, laying the foundation for powerful and reliable web applications.
-		
+	`
+	},
+	{
+		id: 4,
+		title: "Blockchain Wizadry",
+		bio:
+		`	
 		Blockchain Wizardry:
 		As a dedicated blockchain enthusiast, I am proficient in the art of developing decentralized solutions. My skills extend to smart contract development using Solidity, enabling the creation of secure and transparent applications on blockchain platforms.
-		
-
+	`
+	},
+	{
+		id: 5,
+		title: "web3.0 Expertise",
+		bio:
+		`
 		Web3.0 Pioneer:
 		I am at the forefront of the web3.0 revolution, leveraging emerging technologies to build the next generation of decentralized applications. My commitment to staying ahead of the curve ensures that my projects are not just current but future-proof.
 		
@@ -26,11 +44,15 @@ export const aboutMeData = [
 		
 		Let's Build the Future Together:
 		Whether you're seeking a dynamic full-stack developer, a blockchain expert, or a visionary for your web3.0 project, you've come to the right place. Let's collaborate and turn your concepts into groundbreaking digital experiences.
-		
-		Explore my portfolio, and let's embark on a journey to redefine the possibilities of web3.0 together.`
-	   },
+	`
+	},	   
 	{
-		id: 2,
-		bio: "I'm SEBERA JONAS, a Rwandan with a passion for football and a talent for problem-solving. On the field, I excel in teamwork and strategic play. Off the field, I channel my skills into creative solutions and collaborative efforts. Let's score victories together, whether in sports or tackling real-world challenges.",
+		id: 6,
+		title: "Nationality",
+		bio: 
+		`
+		I'm a Rwandan with a passion for football and a talent for problem-solving. On the field, I excel in teamwork and strategic play. Off the field, I channel my skills into creative solutions and collaborative efforts. Let's score victories together, whether in sports or tackling real-world challenges.",
+		Explore my portfolio, and let's embark on a journey to redefine the possibilities of web3.0 together.`
+
 	},
 ];
