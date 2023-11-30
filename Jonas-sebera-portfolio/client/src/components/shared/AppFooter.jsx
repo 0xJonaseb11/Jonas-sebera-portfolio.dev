db79aeff4c2a19fd4c356b8b892474c3a54e5094
jonas-sebera <sebejaz99@gmail.com>
2023-11-30 03:53:55 +0200

    updates

--- Jonas-sebera-portfolio/client/src/components/shared/AppFooter.jsx
+++ Jonas-sebera-portfolio/client/src/components/shared/AppFooter.jsx
@@ -1,127 +1,72 @@
-import Discord from "../../images/discord.jpeg";
-import Dev from "../../images/Dev.png";
-import Medium from "../../images/Medium.png";
-import Quora from "../../images/quora.jpeg";
+import React, { useRef } from 'react';  // Add this line to import `useRef`
+import AppFooterCopyright from './AppFooterCopyright';  // Add this line to import `AppFooterCopyright`
 
-
-import {
-	FiGithub,
-	// eslint-disable-next-line no-unused-vars
-	FiTwitter,
-	FiLinkedin,
-	// eslint-disable-next-line no-unused-vars
-	FiFacebook,
-	FiInstagram,
-	
-} from 'react-icons/fi';
-import AppFooterCopyright from './AppFooterCopyright';
-
+// ... (previous imports)
 
 const CopyAddressButton = ({ walletAddress }) => {
-	const addressRef = useRef(null);
-  
-	const handleCopyClick = () => {
-	  if (addressRef.current) {
-		const textArea = document.createElement('textarea');
-		textArea.value = walletAddress;
-		document.body.appendChild(textArea);
-		textArea.select();
-		document.execCommand('copy');
-		document.body.removeChild(textArea);
-  
-		
-		console.log('Address copied to clipboard');
-	  }
-	};
-  
+  const addressRef = useRef(null);
 
+  const handleCopyClick = () => {
+    if (addressRef.current) {
+      const textArea = document.createElement('textarea');
+      textArea.value = walletAddress;
+      document.body.appendChild(textArea);
+      textArea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textArea);
+
+      window.alert('Address copied to clipboard');
+    }
+  };
+
+  return (
+    <div>
+      <span ref={addressRef}>{walletAddress}</span>
+      <button onClick={handleCopyClick} className='p-2 ml-2 rounded-xl bg-gray-300'> Copy </button>
+    </div>
+  );
+};
 
 const socialLinks = [
-	{
-		id: 1,
-		icon: <FiGithub className="m-2"/>,
-		url: 'https://github.com/Jonas-sebera',
-	},
-	{
-		id: 2,
-		icon: <img src={Discord} alt="Discord Logo" width={45} className="m-2"/>,
-		url: 'https://discord.com/users/1065213095431131218',
-	},
-	{
-		id: 3,
-		icon: <FiLinkedin className="m-2" />,
-		url: 'https://www.linkedin.com/in/sebera-jonas/',
-	},
-	{
-		id: 4,
-		icon: <FiFacebook className="m-2"/>,
-		url: 'https://www.facebook.com/jonaswalker.deborde/',
-	},
-	{
-		id: 5,
-		icon: <FiInstagram className="m-2"/>,
-		url: 'https://www.instagram.com/sebe_j_az99',
-	},
-	
-	{
-		id: 6,
-		icon: <img src={Medium} alt="Medium Logo" width={50} className="m-2"/>,
-		url: 'https://medium.com/@sebejaz99',
-	},
-	{
-		id: 7,
-		icon: <img src={Quora} alt="Quora logo" width={60} className="m-2"/>,
-		url: 'https://www.quora.com/profile/Jaz-1378',
-	},
-	{
-		id: 8,
-		icon: <img src={Dev} alt="Dev Community logo" width={50} className="m-2"/>,
-		url: 'https://dev.to/jonassebera',
-	}
-	
+  // ... (socialLinks array)
 ];
 
-
 const AppFooter = () => {
-	return (
-		<div className="container mx-auto">
-			<div className="pt-20 sm:pt-30 pb-8 mt-20 border-t-2 border-primary-light dark:border-secondary-dark">
-				{/* Footer social links */}
-				<div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
-					<p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-10">
-						Let's go social
-					</p>
-					<ul className="grid grid-cols-2 gap-3 sm:gap-8 md:grid md:grid-cols-4 ml-2">
-						{socialLinks.map((link) => (
-							<a
-								href={link.url}
-								target="__blank"
-								key={link.id}
-								className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-xl bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
-							>
-								<i className="text-xl sm:text-2xl md:text-4xl">
-									{link.icon}
-								</i>
-							</a>
-						))}
-					</ul>
+  return (
+    <div className="container mx-auto">
+      {/* Footer social links */}
+      <div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
+        <p className="text-3xl sm:text-4xl text-primary-dark dark:text-primary-light mb-10">
+          Let's go social
+        </p>
+        <ul className="grid grid-cols-2 gap-3 sm:gap-8 md:grid md:grid-cols-4 ml-2">
+          {socialLinks.map((link) => (
+            <a
+              href={link.url}
+              target="__blank"
+              key={link.id}
+              className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-xl bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
+            >
+              <i className="text-xl sm:text-2xl md:text-4xl">
+                {link.icon}
+              </i>
+            </a>
+          ))}
+        </ul>
 
-					<div className="mt-10 items-center flex flex-col bg-indigo-100 p-8 w-full rounded-3xl">
-						<p className="font-semibold mb-2 text-[#999] text-3xl">We also take coffee please</p>
-						<p className="font-serif">0xcfdEf8165d722DC56a1FD06923de261B5B9d8626</p>
-					</div>
-					
-				</div>
+        <div className="mt-10 items-center flex flex-col bg-indigo-100 p-8 w-full rounded-3xl">
+          <p className="font-semibold mb-2 text-[#999] text-3xl">We also take coffee please</p>
+          <p className="font-serif">
+		  <CopyAddressButton walletAddress="0xcfdEf8165d722DC56a1FD06923de261B5B9d8626" />
+</p>
+        </div>
+        
+      </div>
 
-				<AppFooterCopyright />
-			</div>
-			<div>
-      <span ref={addressRef}>{walletAddress}</span>
-      <button onClick={handleCopyClick}>Copy Address</button>
+      
+      <AppFooterCopyright />
     </div>
-  
-		</div>
-	);
+  );
 };
 
 export default AppFooter;
