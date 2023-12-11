b89dd06578d76be527a5b142f9873e04955fd402
jonas-sebera <sebejaz99@gmail.com>
2023-12-11 07:16:41 +0200

    update resume

--- Jonas-sebera-portfolio/client/src/components/about/AboutCounter.jsx
+++ Jonas-sebera-portfolio/client/src/components/about/AboutCounter.jsx
@@ -31,7 +31,11 @@ const AboutCounter = () => {
 	return (
 		<div className="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
 			<div className="font-general-medium container mx-auto py-20 block sm:flex sm:justify-between items-center">
-			
+				<CounterItem
+					title="Years of experience"
+					counter={<span id="experienceCounter" />}
+					measurement=""
+				/>
 
 
 
