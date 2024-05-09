1c4008ce997c08e06d9b51bff44711d841b2a7af
Jaz-3-0 <sebejaz99@gmail.com>
2024-05-09 16:17:18 +0200

    update init web3

--- Jonas-sebera-portfolio/smart_contract/hardhat.config.js
+++ Jonas-sebera-portfolio/smart_contract/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    }
+  },
+  compilers: {
+    solc: "^0.8.19"
+  }
 };
--- Jonas-sebera-portfolio/smart_contract/scripts/deploy.js
+++ Jonas-sebera-portfolio/smart_contract/scripts/deploy.js
@@ -1,33 +1,29 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const main = async () => {
+  // const accounts = await hre.ethers.provider.listAccounts();
+  // console.log(accounts);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
 
-  await lock.waitForDeployment();
+  const Welcome = await hre.ethers.getContractFactory("Welcome");
+  const welcome = Welcome.deploy();
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log("Deploying Welcome contract.....");
+
+  await welcome.deployed();
+
+  console.log("Welcome contract deployed to address: ", welcome.address);
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+const runMain = async() => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+runMain();
